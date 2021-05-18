ce9f851809e6b04dfb3b94c669bcd36830077089
edurfey <emily.durfey@gmail.com>
2021-05-18 18:53:11 -0500

    Add files via upload

---
+++ Assignment06_Starter.py
@@ -0,0 +1,20 @@
+# ---------------------------------------------------------------------------- #
+# Title: Assignment06
+# Description: Working with functions in a class,
+# Add the each dictionary "row" to a python list "table"
+# ChangeLog (Who,When,What):
+# EDurfey, 05/11/2021, Added scripts as part of Assignment 5
+# EDurfey, 05/17/2021, Modified scripts in order to complete Assignment 6
+# ---------------------------------------------------------------------------- #
+
+# Data ---------------------------------------------------------------------- #
+# Declare variables and constants
+strFileName = "ToDoFile.txt"
+objFile = None
+dicRow = {}
+lstTable = []
+strChoice = ""
+strTask = ""
+strPriority = ""
+strStatus = ""
+
